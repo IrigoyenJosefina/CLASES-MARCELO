6a3a57a389372cca33f34e7dbbb4726a914627ef
IrigoyenJosefina <110865207+IrigoyenJosefina@users.noreply.github.com>
2023-03-31 18:01:50 -0300

    Delete Clase02 directory

--- Clase02/Engine.ts
+++
@@ -1,9 +0,0 @@
-export class Engine {
-    private type: string;
-    public constructor(type: string) {
-      this.type = type;
-    }
-    getEngine() {
-      console.log("Engine type:", this.type);
-    }
-  };
--- Clase02/Tyre.ts
+++
@@ -1,9 +0,0 @@
-export class Tyre {
-    private size: number;
-    public constructor(size: number) {
-      this.size = size;
-    }
-    getSize() {
-      console.log("Tyre size:", this.size);
-    }
-  };
